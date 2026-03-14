60178af0a542ac6eb2478c3b3c64b38688e3f94e
Abdi-Shakur457 <abdishakur457@hotmail.com>
2026-03-14 17:39:02 +0000

    fixed error in index.js file

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const core = required('@actions/core') 
+const core = require('@actions/core') 
 
 
 try { 
