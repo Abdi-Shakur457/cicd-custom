e57a9184902c46d3511ecf88cab3aee2339a5bfc
Abdi-Shakur457 <abdishakur457@hotmail.com>
2026-03-15 14:42:44 +0000

    fixed error in index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+
+try {
+    
+    const nameToGreet = core.getInput('who-to-greet');
+
+    
+    console.log(`Hello, ${nameToGreet}!`);
+} catch (error) {
+    core.setFailed(error.message)
+}
